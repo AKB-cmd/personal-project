17f0e77493a944e5830a4bac896b189fa7d9a090
AKHIL KUMAR BODDAPATI <138310921+Akhil1-art@users.noreply.github.com>
2024-07-21 13:21:28 +0530

    scrape.py

--- scrape.py
+++ scrape.py
@@ -1,12 +1,12 @@
-import fitz
 import PyPDF2
 
 pdf_path = 'C:/Users/Asus/OneDrive/Desktop/New folder/hydfcst.pdf'
 
-# Using PyMuPDF (fitz)
-print("Reading PDF with PyMuPDF (fitz):\n")
-pdf_document = fitz.open(pdf_path)
-for page_num in range(len(pdf_document)):
-    page = pdf_document.load_page(page_num)
-    text = page.get_text()
-    print(f'Page {page_num + 1}:\n{text}\n')
+# Using PyPDF2
+print("Reading PDF with PyPDF2:\n")
+with open(pdf_path, 'rb') as file:
+    pdf_reader = PyPDF2.PdfFileReader(file)
+    for page_num in range(pdf_reader.numPages):
+        page = pdf_reader.getPage(page_num)
+        text = page.extractText()
+        print(f'Page {page_num + 1}:\n{text}\n')
